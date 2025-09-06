b961111c174ec07ed83c2c6682a5e79a6bc08b28
bkj <bkj.322@gmail.com>
2025-09-06 04:53:28 +0000

    [WORKING]

--- bkj_experiments/traj/traj.py
+++ bkj_experiments/traj/traj.py
@@ -47,22 +47,35 @@ class InspectModel:
         return self.model.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=False)
     
     def forward(self, messages, chunk_size=16):
+        # <<
         self.model.reset_hooks()
+        logits, activations = self.model.run_with_cache(
+            self.prep(messages), 
+            padding_side = self.model.tokenizer.padding_side, 
+            names_filter = lambda hook_name: 'resid_post' in hook_name # Is this right?
+        )
+        logits      = logits.to('cpu')
+        activations = activations.to('cpu')
         
-        all_logits      = []
-        all_activations = []
-        for offset in range(0, len(messages), chunk_size):
-            messages_chunk = messages[offset:offset+chunk_size]
-            logits, activations = self.model.run_with_cache(
-                self.prep(messages_chunk), 
-                padding_side = self.model.tokenizer.padding_side, 
-                names_filter = lambda hook_name: 'resid_post' in hook_name # Is this right?
-            )
-            all_logits.append(logits.to('cpu'))
-            all_activations.append(activations.to('cpu'))
+        return logits, activations
+        # --        
+        # all_logits      = []
+        # all_activations = []
+        # for offset in range(0, len(messages), chunk_size):
+        #     self.model.reset_hooks()
+        #     messages_chunk = messages[offset:offset+chunk_size]
+        #     print(f'{len(messages_chunk)} messages')
+        #     logits, activations = self.model.run_with_cache(
+        #         self.prep(messages_chunk), 
+        #         padding_side = self.model.tokenizer.padding_side, 
+        #         names_filter = lambda hook_name: 'resid_post' in hook_name # Is this right?
+        #     )
+        #     all_logits.append(logits.to('cpu'))
+        #     all_activations.append(activations.to('cpu'))
         
-        # breakpoint() # [TODO] get the batch to work
-        return all_logits, all_activations
+        # # breakpoint() # [TODO] get the batch to work
+        # return all_logits, all_activations
+        # >>
 
 
 ins = InspectModel()
@@ -72,7 +85,7 @@ ins = InspectModel()
 
 inpaths = [
     Path(__here__) / 'data/seed_10016.jl',
-    Path(__here__) / 'data/seed_10116.jl',
+    # Path(__here__) / 'data/seed_10116.jl',
 ]
 
 data   = [json.loads(line) for inpath in inpaths for line in inpath.open('r').readlines()]
@@ -106,66 +119,61 @@ df['target_str']  = letter2str(df, 'target')
 assert (df.correct == (df.answer_str == df.target_str)).all()
 assert df.groupby('_qidx').target_str.apply(lambda x: 1 == len(set(x))).values.all()
 
-print(df.head())
+# # --
+# # sanity checks
 
-# --
-# sanity checks
+# # does majority voting help?
+# # yes ... though not be a ton
+# df.correct.mean()
+# df.groupby('_qidx').apply(lambda x: Counter(x.answer_str).most_common(1)[0][0] == x.target_str.values[0]).mean()
 
-# does majority voting help?
-# yes ... though not be a ton
-df.correct.mean()
-df.groupby('_qidx').apply(lambda x: Counter(x.answer_str).most_common(1)[0][0] == x.target_str.values[0]).mean()
+# df["_maj"] = df.groupby('_qidx').answer_str.transform(lambda x: Counter(x).most_common(1)[0][0])
+# df["_n"]   = df.groupby('_qidx').answer_str.transform(lambda x: len(set(x)))
 
-df["_maj"] = df.groupby('_qidx').answer_str.transform(lambda x: Counter(x).most_common(1)[0][0])
-df["_n"]   = df.groupby('_qidx').answer_str.transform(lambda x: len(set(x)))
+# # is consistency correlated with correctness?
+# # yes
+# udf = df.drop_duplicates('_qidx')
+# pd.crosstab(udf._n, udf._maj == udf.target_str, normalize='index')
 
-# is consistency correlated with correctness?
-# yes
-udf = df.drop_duplicates('_qidx')
-pd.crosstab(udf._n, udf._maj == udf.target_str, normalize='index')
+# # [TODO] consistency is highly correlated with accuracy ...
 
-# [TODO] consistency is highly correlated with accuracy ...
-
-df['c'] = df.answer_str == df._maj
+# df['c'] = df.answer_str == df._maj
 
 
-# <<
-# pct of responses in majority class is also correlated with accuracy ... maybe this is better
-# than just unique counts
-a = df.groupby('_qidx').c.mean().values
-b = df.groupby('_qidx').apply(lambda x: x._maj.values[0] == x.target_str.values[0]).values
-z = pd.Series(b).groupby(a.round(2)).mean()
-_ = plt.plot(z.index, z.values)
-show_plot()
-# but then the question is how to estimate "pct of responses in majority class" efficiently
+# # <<
+# # pct of responses in majority class is also correlated with accuracy ... maybe this is better
+# # than just unique counts
+# a = df.groupby('_qidx').c.mean().values
+# b = df.groupby('_qidx').apply(lambda x: x._maj.values[0] == x.target_str.values[0]).values
+# z = pd.Series(b).groupby(a.round(2)).mean()
+# _ = plt.plot(z.index, z.values)
+# show_plot()
+# # but then the question is how to estimate "pct of responses in majority class" efficiently
 
-# [TODO] demonstrate this is actually useful, w/ a large number of samples.  does it get
-#        meaningfully better as you go from 8, 16, 32, ..., 128 samples?
-#        - confounder: temperature, ...
-# [IDEA] train a model to predict class from partial rollouts and then use that compute % majority
-#        class for confidence estimation
-# [IDEA] do some unsupervised thing on partial embeddings to get confidence estimates
-# >>
-
+# # [TODO] demonstrate this is actually useful, w/ a large number of samples.  does it get
+# #        meaningfully better as you go from 8, 16, 32, ..., 128 samples?
+# #        - confounder: temperature, ...
+# # [IDEA] train a model to predict class from partial rollouts and then use that compute % majority
+# #        class for confidence estimation
+# # [IDEA] do some unsupervised thing on partial embeddings to get confidence estimates
+# # [IDEA] can you detect which one is going to be the majority vote by looking at the trajectory?
+# # >>
 
 # --
 # Pick a question
 
 acc  = df.groupby('_qidx').correct.mean()
-acc  = acc[(acc > 0.6) & (acc < 0.8)]
+acc  = acc[(acc > 0.4) & (acc < 0.6)]
 qidx = acc.index[3]
 
 sub  = df[df['_qidx'] == qidx]
+
 
 uanswer_str = sub.answer_str.unique()
 answer2idx  = {a: i for i, a in enumerate(uanswer_str)}
 y           = np.array([answer2idx[a] for a in sub.answer_str.values])
 
 assert len(set(y)) == 2
-
-# <<
-sub = sub.head(16)
-# >>
 
 # --
 # Compute activations
@@ -181,41 +189,39 @@ messages      = [
 
 logits, activations = ins.forward(messages)
 
-# <<
-# while working on batching
-logits = logits[0]
-activations = activations[0]
-# >>
-
-# <<
-# prefix_tokens = ins.n_tokens(ins.prep(messages[0][:2]))
-# --
-prefix_tokens = ins.n_tokens(ins.model.tokenizer.apply_chat_template(messages[0][:2], tokenize=False, add_generation_prompt=False))
-output_tokens = [ins.n_tokens(ins.model.tokenizer.apply_chat_template([message[-1]], tokenize=False, add_generation_prompt=False)) for message in messages]
-# >>
+prefix_tokens = ins.n_tokens(ins.prep(messages[0][:2]))
+output_tokens = [ins.n_tokens(ins.prep([message[-1]])) for message in messages]
 
 acts = activations['blocks.30.hook_resid_post'][:,prefix_tokens:].clone()
 acts = acts.numpy()
 
 breakpoint()
 
+# # <<
+from scipy.spatial.distance import pdist, squareform
+mean_acts = np.array([a[:int(t * 0.1)].mean(axis=0) for a, t in zip(acts, output_tokens)])
 
-# <<
-mean_acts = np.array([a[:int(t * p)].mean(axis=0) for a, t in zip(acts, output_tokens)])
-dist      = squareform(pdist(mean_acts, metric='cosine'))
+mean_acts = mean_acts[np.argsort(y)]
+
+dist      = squareform(pdist(np.sign(mean_acts) * np.sqrt(np.abs(mean_acts)), metric='cosine'))
 _         = heatmap(dist, cmap='viridis')
 show_plot()
-# >>
+# # >>
 
 
 # --
 # Train model
 # This seems to be doing something ...
 
+# [TODO] use multiple layers
+# [TODO] use different aggregations
+# [TODO] preprocessing - there are maybe weird spikes here? clipping? normalizing?
+
+from tqdm import trange
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
 
-def run_one(n_train=2, p=1):
+def run_one(acts, output_tokens, y, n_train=2, p=1):
     idx0 = np.where(y == 0)[0]
     idx1 = np.where(y == 1)[0]
     
@@ -224,16 +230,18 @@ def run_one(n_train=2, p=1):
     valid_sel = ~train_sel
     
     mean_acts = np.array([a[:int(t * p)].mean(axis=0) for a, t in zip(acts, output_tokens)])
+    # mean_acts = mean_acts - mean_acts.mean(axis=0)
+    # mean_acts = mean_acts / np.std(mean_acts, axis=0)
     
     clf   = LogisticRegression(max_iter=10000).fit(mean_acts[train_sel], y[train_sel])
-    y_hat = clf.decision_function(mean_acts[valid_sel])
-    return roc_auc_score(y[valid_sel], y_hat)
+    y_hat = clf.predict_proba(mean_acts[valid_sel])[:,1]
+    return roc_auc_score(y[valid_sel], y_hat), y_hat.sum(), y[valid_sel].sum()
 
-from tqdm import trange
+
 for n_train in range(1, 3):
-    for p in [0.25]:
-        tmp = [run_one(n_train=n_train, p=p) for _ in range(64)]
-        print(n_train, p, np.mean(tmp), np.median(tmp))
+    for p in [0.25, 0.5, 0.75]:
+        tmp, a, b = zip(*[run_one(acts, output_tokens, y, n_train=n_train, p=p) for _ in range(64)])
+        print(n_train, p, np.mean(tmp), np.median(tmp), np.mean(a), np.mean(b))
         _ = plt.plot(np.sort(tmp), label=f'{n_train} {p}')
 
 _ = plt.axhline(0.5, c='red')
